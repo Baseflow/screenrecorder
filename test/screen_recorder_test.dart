9628456bf363dbfc2f543079774fa910f1a22dde
Jonas Uekötter <dev@uekoetter.dev>
2022-11-29 16:22:07 +0100

    fix test compile issues

--- test/screen_recorder_test.dart
+++ test/screen_recorder_test.dart
@@ -1,25 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:screen_recorder/screen_recorder.dart';
 
-class CustomExporter extends Exporter {
-  @override
-  Future<List<int>?> export() {
-    throw UnimplementedError();
-  }
-
-  @override
-  void onNewFrame(Frame frame) {}
-}
+class CustomExporter extends Exporter {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  test('adds one to input values', () {});
 
   test('ScreenRecorderController allows to use custom exporter', () {
     final exporter = CustomExporter();
     final scrennRecorder = ScreenRecorderController(exporter: exporter);
 
-    expect(scrennRecorder._exporter, equals(exporter));
+    expect(scrennRecorder.exporter, equals(exporter));
   });
 }
