1ec1644f213fe9d91b272506f716af199e6b5d83
mbfakourii <mbfakourii@gmail.com>
2024-07-07 11:39:37 +0330

    feat: improve example base last flutter changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:example/sample_animation.dart';
@@ -43,124 +44,131 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_exporting)
-              Center(child: CircularProgressIndicator())
-            else ...[
-              ScreenRecorder(
-                height: 500,
-                width: 500,
-                controller: controller,
-                child: SampleAnimation(),
-              ),
-              if (!_recording && !_exporting)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      controller.start();
-                      setState(() {
-                        _recording = true;
-                      });
-                    },
-                    child: Text('Start'),
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (_exporting)
+                Center(child: CircularProgressIndicator())
+              else ...[
+                ScreenRecorder(
+                  height: 500,
+                  width: 500,
+                  controller: controller,
+                  child: SampleAnimation(),
                 ),
-              if (_recording && !_exporting)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      controller.stop();
-                      setState(() {
-                        _recording = false;
-                      });
-                    },
-                    child: Text('Stop'),
+                if (!_recording && !_exporting)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        controller.start();
+                        setState(() {
+                          _recording = true;
+                        });
+                      },
+                      child: Text('Start'),
+                    ),
                   ),
-                ),
-              if (canExport && !_exporting)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        _exporting = true;
-                      });
-                      var frames = await controller.exporter.exportFrames();
-                      if (frames == null) {
-                        throw Exception();
-                      }
-                      setState(() => _exporting = false);
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            content: SizedBox(
-                              height: 500,
-                              width: 500,
-                              child: ListView.builder(
-                                padding: EdgeInsets.all(8.0),
-                                itemCount: frames.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  final image = frames[index].image;
-                                  return Container(
-                                    height: 150,
-                                    child: Image.memory(
-                                      image.buffer.asUint8List(),
-                                    ),
-                                  );
-                                },
+                if (_recording && !_exporting)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        controller.stop();
+                        setState(() {
+                          _recording = false;
+                        });
+                      },
+                      child: Text('Stop'),
+                    ),
+                  ),
+                if (canExport && !_exporting)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        setState(() {
+                          _exporting = true;
+                        });
+                        var frames = await controller.exporter.exportFrames();
+                        if (frames == null) {
+                          throw Exception();
+                        }
+                        setState(() => _exporting = false);
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: SizedBox(
+                                height: 500,
+                                width: 500,
+                                child: ListView.builder(
+                                  padding: EdgeInsets.all(8.0),
+                                  itemCount: frames.length,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    final image = frames[index].image;
+                                    return Container(
+                                      height: 150,
+                                      child: Image.memory(
+                                        image.buffer.asUint8List(),
+                                      ),
+                                    );
+                                  },
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                    child: Text('Export as frames'),
+                            );
+                          },
+                        );
+                      },
+                      child: Text('Export as frames'),
+                    ),
                   ),
-                ),
-              if (canExport && !_exporting) ...[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        _exporting = true;
-                      });
-                      var gif = await controller.exporter.exportGif();
-                      if (gif == null) {
-                        throw Exception();
-                      }
-                      setState(() => _exporting = false);
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            content: Image.memory(Uint8List.fromList(gif)),
-                          );
-                        },
-                      );
-                    },
-                    child: Text('Export as GIF'),
+                if (canExport && !_exporting) ...[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        setState(() {
+                          _exporting = true;
+                        });
+                        var gif = await controller.exporter.exportGif();
+                        if (gif == null) {
+                          throw Exception();
+                        }
+
+
+                        File('/data/data/com.example.example/cache/my_image.gif').writeAsBytes(Uint8List.fromList(gif));
+
+
+                        setState(() => _exporting = false);
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Image.memory(Uint8List.fromList(gif)),
+                            );
+                          },
+                        );
+                      },
+                      child: Text('Export as GIF'),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        controller.exporter.clear();
-                      });
-                    },
-                    child: Text('Clear recorded data'),
-                  ),
-                )
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          controller.exporter.clear();
+                        });
+                      },
+                      child: Text('Clear recorded data'),
+                    ),
+                  )
+                ]
               ]
-            ]
-          ],
+            ],
+          ),
         ),
       ),
     );
